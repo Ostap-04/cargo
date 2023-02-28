39f1704000a28787d2182f67defa3707a1762905
Yuliia Palamar <yuliyapalamar@gmail.com>
2023-02-28 22:45:21 +0200

    overrided ToSring(), operators > and <

--- cargo.cs
+++ cargo.cs
@@ -15,8 +15,8 @@ namespace cargo
 
         public Cargo()
         {
-            sender_adress = "Остап і Юля ПМі-26";
-            recipient_adress = "Ярошко Сергій Адамович";
+            sender_adress = "немає даних";
+            recipient_adress = "немає даних";
             weight = 0.0;
             distance = 0;
         }
@@ -26,6 +26,18 @@ namespace cargo
             this.recipient_adress = recipient_adress;
             this.weight = weight;
             this.distance = distance;
+        }
+        public override string ToString()
+        {
+            return $"адреса вiдправника: {sender_adress} \nадреса отримувача: {recipient_adress} \nвага: {weight}, вiдстань транспортування:{distance}\n";
+        }
+        public static bool operator< (Cargo left, Cargo right)
+        {
+            return left.weight < right.weight;
+        }
+        public static bool operator> (Cargo left, Cargo right)
+        {
+            return left.weight > right.weight;
         }
     }
 }
