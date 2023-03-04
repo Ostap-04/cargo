8dfff88820f8962cab8d94497ed0995027a8feec
Ostap Hutsal <ostaphutsal04@gmail.com>
2023-03-04 18:55:10 +0200

    second part of task

--- Program.cs
+++ Program.cs
@@ -4,28 +4,52 @@
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             Cargo c1 = new Cargo("Address1", "Address2", 10, 100);
             Cargo c2 = new Cargo("Address5", "Address6", 30, 500);
             Cargo c3 = new Cargo("Address3", "Address4", 20, 300);
             Cargo c4 = new Cargo("Address2", "Address3", 15, 200);
             Cargo c5 = new Cargo("Address4", "Address5", 25, 400);
-            List<Cargo> cargoList = new () { c1, c2, c3, c4, c5 };
-            
-            int size = cargoList.Count;
-            Console.WriteLine("\nсписок вантажiв:");
-            for(int i = 0; i < size; i++) {
-                Console.WriteLine($"\nванжтаж{i + 1}\n{cargoList[i]}");
-            }
+            Cargo c7 = new Cargo("Address2", "Address5", 47, 467);
 
+
+            List<Cargo> cargoList = new() { c1, c2, c3, c4, c5, c7 };
+
+            Cargo c6 = new Cargo().DialogInput();
+            cargoList.Add(c6);
+
+            Cargo.PrintList(cargoList);
+            Console.WriteLine("--------------------------------");
+
+            int size = cargoList.Count;
             int minDeliveryIndex = 0;
-            for(int i = 0; i < size; i++) { 
+            for (int i = 0; i < size; i++)
+            {
                 if (cargoList[i].DeliveryPriceCount < cargoList[minDeliveryIndex].DeliveryPriceCount)
                 {
-                    minDeliveryIndex = i; 
-                    break;
+                    minDeliveryIndex = i;
                 }
             }
-            Console.WriteLine($"\n\nвантаж з найменшою вартiстю доставки: вантаж{minDeliveryIndex+1}\n{cargoList[minDeliveryIndex]}");
+            Console.WriteLine($"\nвантаж з найменшою вартiстю доставки: \n{cargoList[minDeliveryIndex]}");
+
+            List<Cargo> senderList = new List<Cargo>();
+            Console.WriteLine("Введіть адресу відправника: ");
+            string senderTemp = Console.ReadLine();
+            foreach (Cargo cargo in cargoList)
+            {
+                if (cargo == senderTemp)
+                {
+                    senderList.Add(cargo);
+                }
+            }
+            Console.WriteLine("--------------------------------\nАдреса відправник {0}", senderTemp);
+            Cargo.PrintList(senderList);
+
+            Console.WriteLine("--------------------------------\nОператори:\n");
+            Console.WriteLine(c2 - c1);
+            Console.WriteLine(c4 - c5);
+            Console.WriteLine(c1 + 200);
         }
     }
 }
--- cargo.cs
+++ cargo.cs
@@ -9,23 +9,33 @@ namespace cargo
     public class Cargo
     {
         private string senderAddress;
-        private string recipientAddress;
+        private string receiverAddress;
         private double weight;
         private int distance;
 
         public Cargo()
         {
             senderAddress = "немає даних";
-            recipientAddress = "немає даних";
+            receiverAddress = "немає даних";
             weight = 0.0;
             distance = 0;
         }
         public Cargo(string senderAddress, string recipientAddress, double weight, int distance)
         {
             this.senderAddress = senderAddress;
-            this.recipientAddress = recipientAddress;
-            this.weight = weight;
-            this.distance = distance;
+            this.receiverAddress = recipientAddress;
+            if (weight <= 0.0)
+            {
+                Console.WriteLine("Увага!!! Вага повинна бути додатньою!\nЧерез неправильний ввід вага рівна 1");
+                this.weight = 1.0;
+            }
+            else { this.weight = weight; }
+            if (distance <= 0.0)
+            {
+                Console.WriteLine("Увага!!! Дистанція повинна бути додатньою!\nЧерез неправильний ввід дистанція рівна 1");
+                this.distance = 1;
+            }
+            else { this.distance = distance; }
         }
 
         public string SenderAddress
@@ -33,53 +43,92 @@ namespace cargo
             get { return senderAddress; }
             set { senderAddress = value; }
         }
-        public string RecipientAddress
+        public string ReceiverAddress
         {
-            get { return recipientAddress; }
-            set { recipientAddress = value; }
+            get { return receiverAddress; }
+            set { receiverAddress = value; }
         }
         public double Weight
         {
             get { return weight; }
-            set { 
-                if(value > 0.0) weight = value;
-                else weight = 1.0;
-            }
+            set { weight = value > 0.0 ? value : 1.0; }
         }
         public int Distance
         {
             get { return distance; }
-            set {
-                if (value > 0) distance = value;
-                else distance = 1;
-            }
+            set { distance = value > 0 ? value : 1; }
         }
-
-        public double DeliveryPriceCount
+        public decimal DeliveryPriceCount
         {
-            get { return 2.5 * weight * distance; }
+            get { return (decimal)(2.5 * weight * distance); }
         }
 
         public override string ToString()
         {
-            return $"адреса вiдправника: {senderAddress} \nадреса отримувача: {recipientAddress} \nвага: {weight}, " +
-                $"вiдстань транспортування: {distance} \nвартiсть доставки: {DeliveryPriceCount}";
+            return $"адреса вiдправника: {senderAddress} \nадреса отримувача: {receiverAddress} \nвага: {weight}, " +
+                $"вiдстань транспортування: {distance} \nвартiсть доставки: {DeliveryPriceCount}\n";
         }
-        public static bool operator< (Cargo left, Cargo right)
+        public static bool operator <(Cargo left, Cargo right)
         {
             return left.weight < right.weight;
         }
-        public static bool operator> (Cargo left, Cargo right)
+        public static bool operator >(Cargo left, Cargo right)
         {
             return left.weight > right.weight;
         }
-        public static Cargo operator+ (Cargo left, int addDistance)
+        public static bool operator ==(Cargo left, string sender)
         {
-            return new Cargo(left.senderAddress, left.recipientAddress, left.weight, left.distance + addDistance);
+            return left.senderAddress == sender;
         }
-        public static Cargo operator- (Cargo left, int addDistance)
+        public static bool operator !=(Cargo left, string sender)
         {
-            return new Cargo(left.senderAddress, left.recipientAddress, left.weight, left.distance - addDistance);
+            return left.senderAddress != sender;
+        }
+        public static Cargo operator +(Cargo left, int addDistance)
+        {
+            return new Cargo(left.senderAddress, left.receiverAddress, left.weight, left.distance + addDistance);
+        }
+        public static Cargo operator +(Cargo left, Cargo right)
+        {
+            return new Cargo(left.senderAddress, left.receiverAddress, left.weight, left.distance + right.distance);
+        }
+        public static Cargo operator -(Cargo left, int addDistance)
+        {
+            return new Cargo(left.senderAddress, left.receiverAddress, left.weight, left.distance - addDistance);
+        }
+        public static Cargo operator -(Cargo left, Cargo right)
+        {
+            return new Cargo(left.senderAddress, left.receiverAddress, left.weight, left.distance - right.distance);
+        }
+
+        public Cargo DialogInput()
+        {
+            Console.WriteLine("Введіть дані для нового вантажу:");
+
+            Console.Write("Адреса відправника: ");
+            string senderAdd = Console.ReadLine();
+            Console.Write("Адреса отримувача: ");
+            string receiverAdd = Console.ReadLine();
+            Console.Write("Вага: ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.Write("Відстань транспортування: ");
+            int distance = int.Parse(Console.ReadLine());
+
+            return new Cargo(senderAdd, receiverAdd, weight, distance);
+        }
+
+        //public static void PrintCargo(Cargo cargo)
+        //{
+        //    Console.WriteLine(cargo);
+        //}
+        public static void PrintList(List<Cargo> list)
+        {
+            Console.WriteLine("\nCписок вантажiв:\n");
+            foreach (Cargo cargo in list)
+            {
+                //PrintCargo(cargo);
+                Console.WriteLine(cargo);
+            }
         }
     }
 }
