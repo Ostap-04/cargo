cbbcbb5561cb98015e35151589c55100adf03438
Ostap Hutsal <ostaphutsal04@gmail.com>
2023-03-04 15:40:15 +0200

    change container

--- Program.cs
+++ Program.cs
@@ -9,23 +9,23 @@
             Cargo c3 = new Cargo("Address3", "Address4", 20, 300);
             Cargo c4 = new Cargo("Address2", "Address3", 15, 200);
             Cargo c5 = new Cargo("Address4", "Address5", 25, 400);
-            Cargo[] cargoArr = {c1, c2, c3, c4, c5};
+            List<Cargo> cargoList = new () { c1, c2, c3, c4, c5 };
             
-            int size = cargoArr.Length;
+            int size = cargoList.Count;
             Console.WriteLine("\nсписок вантажiв:");
-            for(int i = 0; i < size;i++) {
-                Console.WriteLine($"\nванжтаж{i + 1}\n{cargoArr[i]}");
+            for(int i = 0; i < size; i++) {
+                Console.WriteLine($"\nванжтаж{i + 1}\n{cargoList[i]}");
             }
 
             int minDeliveryIndex = 0;
             for(int i = 0; i < size; i++) { 
-                if (cargoArr[i].deliveryPriceCount < cargoArr[minDeliveryIndex].deliveryPriceCount)
+                if (cargoList[i].deliveryPriceCount < cargoList[minDeliveryIndex].deliveryPriceCount)
                 {
                     minDeliveryIndex = i; 
                     break;
                 }
             }
-            Console.WriteLine($"\n\nвантаж з найменшою вартiстю доставки: вантаж{minDeliveryIndex+1}\n{cargoArr[minDeliveryIndex]}");
+            Console.WriteLine($"\n\nвантаж з найменшою вартiстю доставки: вантаж{minDeliveryIndex+1}\n{cargoList[minDeliveryIndex]}");
         }
     }
 }
